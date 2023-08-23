b760e370e962aa45748f1b09696dab4a4f850107
Peter Rigby <contact@peterrigby.me.uk>
2023-08-23 15:58:18 +0100

    Added minimal template example

---
+++ examples/zest-minimal-template/main.cpp
@@ -0,0 +1,16 @@
+#include <zest.h>
+
+void UpdateCallback(zest_microsecs elapsed, void *user_data) {
+}
+
+int main(void) {
+
+	zest_create_info create_info = zest_CreateInfo();
+
+	zest_Initialise(&create_info);
+	zest_SetUserUpdateCallback(UpdateCallback);
+
+	zest_Start();
+
+	return 0;
+}
