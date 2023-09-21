d9a2519701b12286c01a157ac82ef5936dbb5497
Peter Rigby <contact@peterrigby.me.uk>
2023-09-21 14:06:48 +0100

    Better names for texture/bitmap/framebuffer copying

--- zest.h
+++ zest.h
@@ -2025,9 +2025,9 @@ ZEST_API void zest_UpdateAllTextureDescriptorSets(zest_texture texture);
 ZEST_API void zest_RefreshTextureDescriptors(zest_texture texture);
 ZEST_API void zest_ScheduleTextureReprocess(zest_texture texture);
 ZEST_API void zest_WaitUntilTexturesReprocessed();
-ZEST_API void zest_GrabImageToTexture(zest_frame_buffer_t *src_image, zest_texture texture, int src_x, int src_y, int dst_x, int dst_y, int width, int height);
-ZEST_API void zest_GrabTextureFromTexture(zest_texture src_image, zest_texture target, int src_x, int src_y, int dst_x, int dst_y, int width, int height);
-ZEST_API void zest_GrabImageFromTexture(zest_texture src_image, zest_bitmap_t *image, int src_x, int src_y, int dst_x, int dst_y, int width, int height, zest_bool swap_channel);
+ZEST_API void zest_CopyFramebufferToTexture(zest_frame_buffer_t *src_image, zest_texture texture, int src_x, int src_y, int dst_x, int dst_y, int width, int height);
+ZEST_API void zest_CopyTextureToTexture(zest_texture src_image, zest_texture target, int src_x, int src_y, int dst_x, int dst_y, int width, int height);
+ZEST_API void zest_CopyTextureToBitmap(zest_texture src_image, zest_bitmap_t *image, int src_x, int src_y, int dst_x, int dst_y, int width, int height, zest_bool swap_channel);
 //-- End Images and textures
 
 //-- Fonts
