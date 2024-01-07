fb6a5f6490d895796b32ee51aa733b73e8870862
Peter Rigby <contact@peterrigby.me.uk>
2024-01-07 20:35:56 +0000

    Update impl_imgui_glfw.h

--- implementations/impl_imgui_glfw.h
+++ implementations/impl_imgui_glfw.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <zest.h>
+#define IMGUI_DEFINE_MATH_OPERATORS
 #include "imgui/imgui.h"
 #include <imgui/misc/freetype/imgui_freetype.h>
 #include <imgui/backends/imgui_impl_glfw.h>
