4f3ee847fc56ce28cde9aceddd134d51d6f2baa5
Peter Rigby <contact@peterrigby.me.uk>
2024-02-13 16:14:10 +0000

    Added default pool for read/write textures

--- zest.c
+++ zest.c
@@ -1578,6 +1578,11 @@ void zest__set_default_pool_sizes() {
     usage.property_flags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
     zest_SetDeviceImagePoolSize("Texture Buffer", usage.image_flags, usage.property_flags, 1024, zloc__MEGABYTE(64));
 
+    //Storage Textures For Writing/Reading from
+    usage.image_flags = VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
+    usage.property_flags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+    zest_SetDeviceImagePoolSize("Texture Read/Write", usage.image_flags, usage.property_flags, 1024, zloc__MEGABYTE(64));
+
     //Render targets
     usage.image_flags = VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
     usage.property_flags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
