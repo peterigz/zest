e1b7f287551e910513e52eced4ef1d4adeaf2892
Peter Rigby <contact@peterrigby.me.uk>
2024-12-01 18:31:24 +0000

    Update zest-timelinefx-c.c

--- examples/Simple/zest-timelinefx-c/zest-timelinefx-c.c
+++ examples/Simple/zest-timelinefx-c/zest-timelinefx-c.c
@@ -382,7 +382,7 @@ int main(void) {
     //Make a config struct where you can configure zest with some options
     zest_create_info_t create_info = zest_CreateInfo();
     create_info.log_path = "./";
-    //ZEST__UNFLAG(create_info.flags, zest_init_flag_enable_vsync);
+    ZEST__UNFLAG(create_info.flags, zest_init_flag_enable_vsync);
     ZEST__FLAG(create_info.flags, zest_init_flag_log_validation_errors_to_console);
 
     tfx_InitialiseTimelineFX(tfx_GetDefaultThreadCount(), 128 * 1024 * 1024);
