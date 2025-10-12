5bdda29c395f27a21b8093aa68488c9a639ac457
Peter Rigby <contact@peterrigby.me.uk>
2025-10-12 15:25:03 +0100

    Implemented stb rect packer

--- zest.h
+++ zest.h
@@ -64,6 +64,9 @@
     [Debug_Helpers]                     Functions for debugging and outputting queues to the console.
   	[Command_buffer_functions]          GPU command buffer commands
 
+	--3rd Party Libraries
+	[stb_rect_header]
+
  	--Implementations 
  	[Zloc_allocator] 
  	[Pico_png_decoder]
@@ -5873,6 +5876,63 @@ ZEST_API void zest_cmd_DrawLayerInstruction(const zest_command_list command_list
 //a draw routine callback function
 ZEST_API void zest_cmd_DrawIndexed(const zest_command_list command_list, zest_uint index_count, zest_uint instance_count, zest_uint first_index, int32_t vertex_offset, zest_uint first_instance);
 
+// 3rd party libraries
+// [stb_rect_header]
+// stb_rect_pack.h - v1.01 - public domain - rectangle packing
+// Sean Barrett 2014
+// I changed stbrp to zestrp to integrate it more cleanly and make it private to zest and avoid any conflicts anyone
+// wants to use it separately in their own projects that use zest. Removed most of the comments for compactness.
+// Check out the original library here: https://github.com/nothings/stb/blob/master/stb_rect_pack.h
+typedef struct zestrp_context zestrp_context;
+typedef struct zestrp_node    zestrp_node;
+typedef struct zestrp_rect    zestrp_rect;
+
+typedef int            zestrp_coord;
+
+#define ZESTRP__MAXVAL  0x7fffffff
+
+ZEST_PRIVATE int zestrp_pack_rects (zestrp_context *context, zestrp_rect *rects, int num_rects);
+
+struct zestrp_rect
+{
+	int            id;
+	zestrp_coord    w, h;
+	zestrp_coord    x, y;
+	int            was_packed;  // non-zero if valid packing
+}; // 16 bytes, nominally
+
+ZEST_PRIVATE void zestrp_init_target (zestrp_context *context, int width, int height, zestrp_node *nodes, int num_nodes);
+ZEST_PRIVATE void zestrp_setup_allow_out_of_mem (zestrp_context *context, int allow_out_of_mem);
+ZEST_PRIVATE void zestrp_setup_heuristic (zestrp_context *context, int heuristic);
+
+enum
+{
+	ZESTRP_HEURISTIC_Skyline_default=0,
+	ZESTRP_HEURISTIC_Skyline_BL_sortHeight = ZESTRP_HEURISTIC_Skyline_default,
+	ZESTRP_HEURISTIC_Skyline_BF_sortHeight
+};
+
+struct zestrp_node
+{
+	zestrp_coord  x,y;
+	zestrp_node  *next;
+};
+
+struct zestrp_context
+{
+	int width;
+	int height;
+	int align;
+	int init_mode;
+	int heuristic;
+	int num_nodes;
+	zestrp_node *active_head;
+	zestrp_node *free_head;
+	zestrp_node extra[2]; // we allocate two extra nodes so optimal user-node-count is 'width' not 'width+2'
+};
+
+// -- End stb_rect_header
+
 #ifdef __cplusplus
 }
 #endif
@@ -7075,6 +7135,361 @@ zest_bitmap zest_LoadPNG(zest_context context, const char *filename) {
 	return bitmap;
 }
 // --End Pico_png_decoder
+
+// [stb_rect_pack_implementation]
+#ifdef _MSC_VER
+#define ZESTRP__NOTUSED(v)  (void)(v)
+#define ZESTRP__CDECL       __cdecl
+#else
+#define ZESTRP__NOTUSED(v)  (void)sizeof(v)
+#define ZESTRP__CDECL
+#endif
+
+enum
+{
+	ZESTRP__INIT_skyline = 1
+};
+
+ZEST_PRIVATE void zestrp_setup_heuristic(zestrp_context *context, int heuristic)
+{
+	switch (context->init_mode) {
+		case ZESTRP__INIT_skyline:
+			ZEST_ASSERT(heuristic == ZESTRP_HEURISTIC_Skyline_BL_sortHeight || heuristic == ZESTRP_HEURISTIC_Skyline_BF_sortHeight);
+			context->heuristic = heuristic;
+			break;
+		default:
+			ZEST_ASSERT(0);
+	}
+}
+
+ZEST_PRIVATE void zestrp_setup_allow_out_of_mem(zestrp_context *context, int allow_out_of_mem)
+{
+	if (allow_out_of_mem)
+		context->align = 1;
+	else {
+		context->align = (context->width + context->num_nodes-1) / context->num_nodes;
+	}
+}
+
+ZEST_PRIVATE void zestrp_init_target(zestrp_context *context, int width, int height, zestrp_node *nodes, int num_nodes)
+{
+	int i;
+
+	for (i=0; i < num_nodes-1; ++i)
+		nodes[i].next = &nodes[i+1];
+	nodes[i].next = NULL;
+	context->init_mode = ZESTRP__INIT_skyline;
+	context->heuristic = ZESTRP_HEURISTIC_Skyline_default;
+	context->free_head = &nodes[0];
+	context->active_head = &context->extra[0];
+	context->width = width;
+	context->height = height;
+	context->num_nodes = num_nodes;
+	zestrp_setup_allow_out_of_mem(context, 0);
+
+	context->extra[0].x = 0;
+	context->extra[0].y = 0;
+	context->extra[0].next = &context->extra[1];
+	context->extra[1].x = (zestrp_coord) width;
+	context->extra[1].y = (1<<30);
+	context->extra[1].next = NULL;
+}
+
+static int zestrp__skyline_find_min_y(zestrp_context *c, zestrp_node *first, int x0, int width, int *pwaste) {
+	zestrp_node *node = first;
+	int x1 = x0 + width;
+	int min_y, visited_width, waste_area;
+
+	ZESTRP__NOTUSED(c);
+
+	ZEST_ASSERT(first->x <= x0);
+
+	#if 0
+	// skip in case we're past the node
+	while (node->next->x <= x0)
+		++node;
+	#else
+	ZEST_ASSERT(node->next->x > x0); // we ended up handling this in the caller for efficiency
+	#endif
+
+	ZEST_ASSERT(node->x <= x0);
+
+	min_y = 0;
+	waste_area = 0;
+	visited_width = 0;
+	while (node->x < x1) {
+		if (node->y > min_y) {
+			waste_area += visited_width * (node->y - min_y);
+			min_y = node->y;
+			if (node->x < x0)
+				visited_width += node->next->x - x0;
+			else
+				visited_width += node->next->x - node->x;
+		} else {
+			int under_width = node->next->x - node->x;
+			if (under_width + visited_width > width)
+				under_width = width - visited_width;
+			waste_area += under_width * (min_y - node->y);
+			visited_width += under_width;
+		}
+		node = node->next;
+	}
+
+	*pwaste = waste_area;
+	return min_y;
+}
+
+typedef struct
+{
+	int x,y;
+	zestrp_node **prev_link;
+} zestrp__findresult;
+
+static zestrp__findresult zestrp__skyline_find_best_pos(zestrp_context *c, int width, int height)
+{
+	int best_waste = (1<<30), best_x, best_y = (1 << 30);
+	zestrp__findresult fr;
+	zestrp_node **prev, *node, *tail, **best = NULL;
+
+	width = (width + c->align - 1);
+	width -= width % c->align;
+	ZEST_ASSERT(width % c->align == 0);
+
+	if (width > c->width || height > c->height) {
+		fr.prev_link = NULL;
+		fr.x = fr.y = 0;
+		return fr;
+	}
+
+	node = c->active_head;
+	prev = &c->active_head;
+	while (node->x + width <= c->width) {
+		int y,waste;
+		y = zestrp__skyline_find_min_y(c, node, node->x, width, &waste);
+		if (c->heuristic == ZESTRP_HEURISTIC_Skyline_BL_sortHeight) { // actually just want to test BL
+			// bottom left
+			if (y < best_y) {
+				best_y = y;
+				best = prev;
+			}
+		} else {
+			// best-fit
+			if (y + height <= c->height) {
+				// can only use it if it first vertically
+				if (y < best_y || (y == best_y && waste < best_waste)) {
+					best_y = y;
+					best_waste = waste;
+					best = prev;
+				}
+			}
+		}
+		prev = &node->next;
+		node = node->next;
+	}
+
+	best_x = (best == NULL) ? 0 : (*best)->x;
+
+	if (c->heuristic == ZESTRP_HEURISTIC_Skyline_BF_sortHeight) {
+		tail = c->active_head;
+		node = c->active_head;
+		prev = &c->active_head;
+		// find first node that's admissible
+		while (tail->x < width)
+			tail = tail->next;
+		while (tail) {
+			int xpos = tail->x - width;
+			int y,waste;
+			ZEST_ASSERT(xpos >= 0);
+			// find the left position that matches this
+			while (node->next->x <= xpos) {
+				prev = &node->next;
+				node = node->next;
+			}
+			ZEST_ASSERT(node->next->x > xpos && node->x <= xpos);
+			y = zestrp__skyline_find_min_y(c, node, xpos, width, &waste);
+			if (y + height <= c->height) {
+				if (y <= best_y) {
+					if (y < best_y || waste < best_waste || (waste==best_waste && xpos < best_x)) {
+						best_x = xpos;
+						ZEST_ASSERT(y <= best_y);
+						best_y = y;
+						best_waste = waste;
+						best = prev;
+					}
+				}
+			}
+			tail = tail->next;
+		}
+	}
+
+	fr.prev_link = best;
+	fr.x = best_x;
+	fr.y = best_y;
+	return fr;
+}
+
+static zestrp__findresult zestrp__skyline_pack_rectangle(zestrp_context *context, int width, int height)
+{
+	zestrp__findresult res = zestrp__skyline_find_best_pos(context, width, height);
+	zestrp_node *node, *cur;
+
+	if (res.prev_link == NULL || res.y + height > context->height || context->free_head == NULL) {
+		res.prev_link = NULL;
+		return res;
+	}
+
+	node = context->free_head;
+	node->x = (zestrp_coord) res.x;
+	node->y = (zestrp_coord) (res.y + height);
+
+	context->free_head = node->next;
+
+	cur = *res.prev_link;
+	if (cur->x < res.x) {
+		zestrp_node *next = cur->next;
+		cur->next = node;
+		cur = next;
+	} else {
+		*res.prev_link = node;
+	}
+
+	while (cur->next && cur->next->x <= res.x + width) {
+		zestrp_node *next = cur->next;
+		cur->next = context->free_head;
+		context->free_head = cur;
+		cur = next;
+	}
+
+	node->next = cur;
+
+	if (cur->x < res.x + width)
+		cur->x = (zestrp_coord) (res.x + width);
+
+	#ifdef _DEBUG
+	cur = context->active_head;
+	while (cur->x < context->width) {
+		ZEST_ASSERT(cur->x < cur->next->x);
+		cur = cur->next;
+	}
+	ZEST_ASSERT(cur->next == NULL);
+
+	{
+		int count=0;
+		cur = context->active_head;
+		while (cur) {
+			cur = cur->next;
+			++count;
+		}
+		cur = context->free_head;
+		while (cur) {
+			cur = cur->next;
+			++count;
+		}
+		ZEST_ASSERT(count == context->num_nodes+2);
+	}
+	#endif
+
+	return res;
+}
+
+static int ZESTRP__CDECL rect_height_compare(const void *a, const void *b)
+{
+	const zestrp_rect *p = (const zestrp_rect *) a;
+	const zestrp_rect *q = (const zestrp_rect *) b;
+	if (p->h > q->h)
+		return -1;
+	if (p->h < q->h)
+		return  1;
+	return (p->w > q->w) ? -1 : (p->w < q->w);
+}
+
+static int ZESTRP__CDECL rect_original_order(const void *a, const void *b)
+{
+	const zestrp_rect *p = (const zestrp_rect *) a;
+	const zestrp_rect *q = (const zestrp_rect *) b;
+	return (p->was_packed < q->was_packed) ? -1 : (p->was_packed > q->was_packed);
+}
+
+ZEST_PRIVATE int zestrp_pack_rects(zestrp_context *context, zestrp_rect *rects, int num_rects)
+{
+	int i, all_rects_packed = 1;
+
+	for (i=0; i < num_rects; ++i) {
+		rects[i].was_packed = i;
+	}
+
+	qsort(rects, num_rects, sizeof(rects[0]), rect_height_compare);
+
+	for (i=0; i < num_rects; ++i) {
+		if (rects[i].w == 0 || rects[i].h == 0) {
+			rects[i].x = rects[i].y = 0;  // empty rect needs no space
+		} else {
+			zestrp__findresult fr = zestrp__skyline_pack_rectangle(context, rects[i].w, rects[i].h);
+			if (fr.prev_link) {
+				rects[i].x = (zestrp_coord) fr.x;
+				rects[i].y = (zestrp_coord) fr.y;
+			} else {
+				rects[i].x = rects[i].y = ZESTRP__MAXVAL;
+			}
+		}
+	}
+
+	qsort(rects, num_rects, sizeof(rects[0]), rect_original_order);
+
+	for (i=0; i < num_rects; ++i) {
+		rects[i].was_packed = !(rects[i].x == ZESTRP__MAXVAL && rects[i].y == ZESTRP__MAXVAL);
+		if (!rects[i].was_packed)
+			all_rects_packed = 0;
+	}
+
+	return all_rects_packed;
+}
+
+/*
+//These are the licenses for the stb rect pack sections only
+------------------------------------------------------------------------------
+This software is available under 2 licenses -- choose whichever you prefer.
+------------------------------------------------------------------------------
+ALTERNATIVE A - MIT License
+Copyright (c) 2017 Sean Barrett
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+------------------------------------------------------------------------------
+ALTERNATIVE B - Public Domain (www.unlicense.org)
+This is free and unencumbered software released into the public domain.
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute this
+software, either in source code form or as a compiled binary, for any purpose,
+commercial or non-commercial, and by any means.
+In jurisdictions that recognize copyright laws, the author or authors of this
+software dedicate any and all copyright interest in the software to the public
+domain. We make this dedication for the benefit of the public at large and to
+the detriment of our heirs and successors. We intend this dedication to be an
+overt act of relinquishment in perpetuity of all present and future rights to
+this software under copyright law.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------
+*/
+
+// -- End stb_rect_pack_implementation
+
 
 #include "zest_impl.h"
 
