4fce707fd292a69bdce78dce5aa91bf1a70b0c1a
Peter Rigby <contact@peterrigby.me.uk>
2025-12-27 22:27:12 +0000

    Find glfw on my system for ease

--- cmake/FindGLFW3.cmake
+++ cmake/FindGLFW3.cmake
@@ -18,12 +18,14 @@ set( _glfw3_HEADER_SEARCH_DIRS
 "/usr/include"
 "/usr/local/include"
 "${CMAKE_SOURCE_DIR}/includes"
-"C:/Program Files (x86)/glfw/include" )
+"C:/Program Files (x86)/glfw/include"
+"E:/glfw-3.3.8/include" )
 set( _glfw3_LIB_SEARCH_DIRS
 "/usr/lib"
 "/usr/local/lib"
 "${CMAKE_SOURCE_DIR}/lib"
-"C:/Program Files (x86)/glfw/lib-msvc110" )
+"C:/Program Files (x86)/glfw/lib-msvc110"
+"E:/glfw-3.3.8/lib" )
 
 # Check environment for root search directory
 set( _glfw3_ENV_ROOT $ENV{GLFW3_ROOT} )
