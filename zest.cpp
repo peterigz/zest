9242ea63ecb4d5ae3541779de331168bf8b1d3d8
Peter Rigby <contact@peterrigby.me.uk>
2026-01-14 15:43:14 +0000

    Add zest as it's own project that compiles to a lib

---
+++ zest.cpp
@@ -0,0 +1,6 @@
+// Zest static library implementation
+// This file compiles zest.h into a static library (.lib)
+
+#define ZEST_IMPLEMENTATION
+#define ZEST_VULKAN_IMPLEMENTATION
+#include "zest.h"
